415711257f22db724186997768d9ab982204deb5
KaryGzz <karyme.gonzalezaln@uanl.edu.mx>
2024-11-11 20:03:57 -0600

    Virus-total.psm1

--- PIAE1/Virus-total.psm1
+++ PIAE1/Virus-total.psm1
@@ -21,56 +21,49 @@ Set-StrictMode -Version Latest
 #>
 
 $apiKey = "eae7f14254551615300ede2d104b88395322289543319e0675782b7ddc06cf85"
+$logFile = "C:\Users\ruta-a-cambiar\Desktop\VirusTotal_Report.txt"
+
 function Analyze-File {
     param (
         [string]$FilePath
     )
 
-    
     if (-Not (Test-Path -Path $FilePath)) {
         Write-Output "El archivo no se encuentra en la ruta especificada: $FilePath"
         return
     }
 
-    
     $hash = (Get-FileHash -Path $FilePath -Algorithm SHA256).Hash
-
-    
     $url = "https://www.virustotal.com/api/v3/files/$($hash)"
     $headers = @{
         "x-apikey" = $apiKey
     }
 
-    
     try {
         $response = Invoke-RestMethod -Uri $url -Method Get -Headers $headers
 
-        
-        Write-Output "Hash: $($response.data.id)"
-        Write-Output "Resultado: $($response.data.attributes.last_analysis_stats)"
+        $result = "Hash: $($response.data.id)`nResultado: $($response.data.attributes.last_analysis_stats)`n"
+        Write-Output $result
+        $result | Out-File -Append -FilePath $logFile
     } catch {
-        Write-Output "Error al consultar la API: $_"
+        $error = "Error al consultar la API para el archivo $FilePath: $_"
+        Write-Output $error
+        $error | Out-File -Append -FilePath $logFile
     }
 }
-
-Analyze-File -FilePath "C:\Users\Ramiro Gonzalez\Desktop\horario.exe"
-
 
 function Analyze-Directory {
     param (
         [string]$DirectoryPath
     )
 
-    
     if (-Not (Test-Path -Path $DirectoryPath)) {
         Write-Output "El directorio no se encuentra en la ruta especificada: $DirectoryPath"
         return
     }
 
-    
     $files = Get-ChildItem -Path $DirectoryPath -File
 
-    
     foreach ($file in $files) {
         Write-Output "Analizando archivo: $($file.FullName)"
         Analyze-File -FilePath $file.FullName
@@ -78,5 +71,7 @@ function Analyze-Directory {
 }
 
 # Llamar a la función para analizar todos los archivos en el directorio
-# Cambia la ruta al directorio que deseas analizar si es necesario
-Analyze-Directory -DirectoryPath "C:\Users\Ramiro Gonzalez\Desktop"
+Analyze-Directory -DirectoryPath "C:\Users\ruta-a-cambiar\Desktop"
+
+# Verificación del reporte
+Write-Output "El reporte se ha guardado en: $logFile"
